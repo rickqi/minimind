924b77df8a76a1469978c9cf5f3443becf1169db
rick qi <rick.zhang@cigancmb.com>
2026-08-05 11:20:59 +0800

    [chore] remove temp pipeline A runner script

--- scripts/run_pipeline_a.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /mnt/d/codes/minimind
-export PYTHONIOENCODING=utf-8
-rm -f dataset/pretrain_medical.jsonl out/medical_pretrain_report.json
-echo "=== run pipeline A (3 sources) ==="
-python3 -u scripts/build_medical_pretrain.py 2>&1 | tail -20
-
